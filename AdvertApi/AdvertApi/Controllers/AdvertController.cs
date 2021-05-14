8bcc1954ba977cbb8af9f780e988e14d9deb04b4
BerrFranz <https://github.com/BerrFranz/>
2021-05-14 13:43:19 +0200

    Init First Relase

---
+++ AdvertApi/AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Services;
+using AdvertApi.Models;
+
+namespace AdvertApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.AddAsync(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.ConfirmAsync(model);
+                //await RaiseAdvertConfirmedMessage(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return new OkResult();
+        }
+
+        //private async Task RaiseAdvertConfirmedMessage(ConfirmAdvertModel model)
+        //{
+        //    var topicArn = Configuration.GetValue<string>("TopicArn");
+        //    var dbModel = await _advertStorageService.GetByIdAsync(model.Id);
+
+        //    using (var client = new AmazonSimpleNotificationServiceClient())
+        //    {
+        //        var message = new AdvertConfirmedMessage
+        //        {
+        //            Id = model.Id,
+        //            Title = dbModel.Title
+        //        };
+
+        //        var messageJson = JsonConvert.SerializeObject(message);
+        //        await client.PublishAsync(topicArn, messageJson);
+        //    }
+        //}
+
+    }
+}
---
+++ AdvertApi/AdvertApi/Entities/AdvertProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using AutoMapper;
+
+namespace AdvertApi.Entities
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            CreateMap<AdvertDbModel, AdvertModel>().ReverseMap();
+        }
+    }
+}
